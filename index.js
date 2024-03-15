ca542567e10d5a32acdc262d56a71aff03c12cc3
Datta Jadhav <dattajadhav7263@gmail.com>
2024-03-16 01:28:23 +0530

    create and listing api done

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
+const todoDB = [];
 
-app.get('/', (req, res)=>{
-    return res.write('Working');
+app.post('/create-todo', (req, res)=>{
+    const { task } = req.body;
+    const randomId = Math.floor(Math.random() * 1000);
+    // console.log(randomId);
+    const todoItem = {
+        id: randomId,
+        task: task
+    };
+
+    todoDB.push(todoItem);
+
+    const response = {
+        'message':'Todo created successfully done!',
+        'data': todoItem
+    }
+    
+    return res.status(201).json(response);
 });
+
+app.get('/todo-list', (req,res)=>{
+    return res.status(200).json(todoDB);
+})
 
 
 
